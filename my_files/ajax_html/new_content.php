76807a5dfe7a5796c937a07152c5595eaf3dd0d6
Edward Reyes <edward.reyes.-nd@abc.com>
2021-10-01 10:59:07 -0700

    testing debugging features

--- my_files/ajax_html/new_content.php
+++ my_files/ajax_html/new_content.php
@@ -1,4 +1,7 @@
 <?php 
 $var = 'new content';
+/** for debugging */
+// echo $var;
+// return;
 ?>
-This is the <strong><?php echo $var ?></strong> which has been loaded by Ajax.
+<span style="color: blue;">This is the <strong><?php echo $var ?></strong> which has been loaded by Ajax.</span>
